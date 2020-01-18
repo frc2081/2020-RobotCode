cb06fceac8da2597fa933849b2a9122dd4215b69
blzzrd <blzzrd_git@nym.hush.com>
2020-01-17 19:21:37 -0600

    Adds basic robot framework

---
+++ src/robot.py
@@ -0,0 +1,19 @@
+import wpilib
+import wpilib.drive
+
+
+class MyRobot(wpilib.TimedRobot):
+    def robotInit(self):
+
+    def robotPeriodic(self):
+
+    def autonomousInit(self):
+
+    def autonomousPeriodic(self):
+
+    def teleoPeriodic(self):
+    
+    def teleopPeriodic(self):
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
