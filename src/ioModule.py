8865dbbb291efe679cfb6927ffb27fcd452655c9
MrPooderMan <peterlolsen@icloud.com>
2020-01-25 15:58:53 -0600

    Starts IO work

--- src/ioModule.py
+++ src/ioModule.py
@@ -1,6 +1,12 @@
+import wpilib
+import rev
+
 class io:
     def __init__(self):
-        pass
+        self.swerveLFDMotor = rev.CANSparkMax(1, rev.MotorType.kBrushless)
+        self.swerveRFDMotor = rev.CANSparkMax(2, rev.MotorType.kBrushless)
+
         
     def periodic(self, interfaces):
+        self.swerveLFDMotor.set(.5)
         print("io Periodic function called")
