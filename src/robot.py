980ab488baa0b8db2c1fccf1a627ebd5aa0f9e65
SirBlobfis <46615149+SirBlobfis@users.noreply.github.com>
2020-01-28 19:37:45 -0600

    adds controlller functionality

--- src/robot.py
+++ src/robot.py
@@ -4,18 +4,22 @@ import interfacesModule
 import ioModule
 import subSystemOneModule
 import subSystemTwoModule
+import controllerManager
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
-        self.interfaces = interfacesModule.interface()
-        self.io = ioModule.io()
-        self.subSysOne = subSystemOneModule.subSystemOne()
-        self.subSysTwo = subSystemTwoModule.subSystemTwo()
+        #self.interfaces = interfacesModule.interface()
+        #self.io = ioModule.io()
+        #self.subSysOne = subSystemOneModule.subSystemOne()
+        #self.subSysTwo = subSystemTwoModule.subSystemTwo()
+        self.controllerManagerInst = controllerManager.controllerManager()
+        self.controllerManagerInst.controllerInit()
 
     def robotPeriodic(self):
-        self.io.periodic(self.interfaces)
-        self.subSysOne.periodic(self.interfaces)
-        self.subSysTwo.periodic(self.interfaces)
+        #self.io.periodic(self.interfaces)
+        #self.subSysOne.periodic(self.interfaces)
+        #self.subSysTwo.periodic(self.interfaces)
+        pass
 
     def autonomousInit(self):
         pass
@@ -24,7 +28,8 @@ class MyRobot(wpilib.TimedRobot):
         pass
 
     def teleopPeriodic(self):
-        pass
+        self.controllerManagerInst.controllerManagerPeriodic()
+        self.controllerManagerInst.controllerManagerSmartDashboard()
 
 if __name__ == "__main__":
     wpilib.run(MyRobot)
