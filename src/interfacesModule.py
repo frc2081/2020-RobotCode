46d164d2b55ff021d60b8b1a9513126b79a44530
SirBlobfis <46615149+SirBlobfis@users.noreply.github.com>
2020-01-30 19:02:10 -0600

    adds motor outputs to interfaces

--- src/interfacesModule.py
+++ src/interfacesModule.py
@@ -1,6 +1,10 @@
 import wpilib
 
 class interfaces:
+
+    #CONTROLLER INPUTS
+    #\/---------\/
+
     #driver controller
     dTurn = 0
     dMoveX = 0
@@ -21,6 +25,40 @@ class interfaces:
     mShootTrenchAuto = False
     mReverseIndexer = False
     mStartShooter = 0
+
+    #MOTORS
+    #\/---------\/
+
+    #swerve
+    driveMotor1 = 0
+    driveMotor2 = 0
+    driveMotor3 = 0
+    driveMotor4 = 0
+    turnMotor1 = 0
+    turnMotor2 = 0
+    turnMotor3 = 0
+    turnMotor4 = 0
+
+    #shooter
+    #shooter speed is in rpm
+    shooterTopSpeed = 0
+    shooterBottomSpeed = 0
+    indexerAngle = 0
+    photoSensor = False
+
+    #control panel
+    spinnerUpDown = 0
+    spinnerSpeed = 0
+
+    #intake
+    #Release solenoid?
+    intakeWheelSpeed = 0
+
+    #climber
+    #climber release is an angle (maybe)
+    climberRelease = 0
+    climberWinchSpeed = 0
+
 
     def __init__(self):
         pass
